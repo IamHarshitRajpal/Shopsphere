9c1e18433c0837e5a9467fecbd25c67dda148715
IamHarshitRajpal <harshitrajpal25082003@gmail.com>
2026-07-24 03:21:12 +0530

    Adding find by email and find order by userId

--- src/main/java/com/shopesphere/shopsphere/Repository/OrderRepository.java
+++ src/main/java/com/shopesphere/shopsphere/Repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package com.shopesphere.shopsphere.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shopesphere.shopsphere.Entity.OrderEntity;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
-
+    public List<OrderEntity> findByUserUserId(Long userId);
 }
--- src/main/java/com/shopesphere/shopsphere/Repository/UserRepository.java
+++ src/main/java/com/shopesphere/shopsphere/Repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.shopesphere.shopsphere.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shopesphere.shopsphere.Entity.UserEntity;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
+     Optional<UserEntity> findByEmail(String email);
 }
